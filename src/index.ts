e37411b38348b093ed0dbe0d4d151695378af51a
Oleg Konan <o.konan@softteco.com>
2022-07-24 18:13:46 +0400

    adde more commands

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,23 @@ const token = process.env.TELEGRAM_API_TOKEN as string;
 
 const bot = new Telegraf(token);
 
+bot.start((ctx) => {
+  ctx.reply(`Hello ${ctx.from.first_name}!`);
+  ctx.reply(
+    'Here you can translate any phrase to georgian language. Just send your message'
+  );
+  ctx.reply('To stop bot send /quit');
+  ctx.reply('For help send /help');
+});
+
+bot.command('/quit', (ctx) => ctx.leaveChat());
+
+bot.help((ctx) => {
+  ctx.reply('Send /start to receive a greeting');
+  ctx.reply('Send /keyboard to receive a message with a keyboard');
+  ctx.reply('Send /quit to stop the bot');
+});
+
 bot.on('text', async (ctx) => {
   const text = ctx.message.text.toLowerCase();
   const cachedResult = await history.find(text);
