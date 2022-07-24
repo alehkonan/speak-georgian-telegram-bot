1152069f5e17e660c4d5b55ab9e4125c480687ec
Oleg Konan <o.konan@softteco.com>
2022-07-24 17:32:13 +0400

    added error handling

--- src/services/history.ts
+++ src/services/history.ts
@@ -13,15 +13,22 @@ type History = {
 
 export const history = {
   find: async (text: string) => {
-    const file = await readFile(fileUrl);
-    const historyJson = JSON.parse(file.toString()) as History;
-    return historyJson.records.find((record) => record.from === text)?.to;
+    try {
+      const file = await readFile(fileUrl);
+      const historyJson = JSON.parse(file.toString()) as History;
+      return historyJson.records.find((record) => record.from === text)?.to;
+    } catch (error) {
+      console.error(error);
+    }
   },
   write: async (from: string, to: string) => {
-    const file = await readFile(fileUrl);
-    const historyJson = JSON.parse(file.toString());
-    historyJson.records.push({ from, to });
-    await writeFile(fileUrl, JSON.stringify(historyJson));
-    console.log(historyJson);
+    try {
+      const file = await readFile(fileUrl);
+      const historyJson = JSON.parse(file.toString());
+      historyJson.records.push({ from, to });
+      await writeFile(fileUrl, JSON.stringify(historyJson));
+    } catch (error) {
+      console.error(error);
+    }
   },
 };
