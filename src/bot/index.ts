e9a262e7cb0d9edad96fc4ffc5e9e7b6db014af5
Oleg Konan <o.konan@softteco.com>
2022-09-04 16:42:25 +0400

    experiment fix

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -33,33 +33,34 @@ bot.on('text', async (ctx) => {
 
     if (translationInHistory) {
       await ctx.reply(translationInHistory?.to);
-      const user = await User.findOne({ userId: from.id });
+      await User.findOneAndUpdate(
+        { userId: from.id, 'history.word': translationInHistory },
+        { $inc: { 'history.$.requested': 1 } }
+      );
 
-      if (user) {
-        console.log(`User ${user.id} found in database`);
-        console.log('user: ', JSON.stringify(user));
-        console.log('word in history: ', JSON.stringify(translationInHistory));
-        const matchWord = user.history.find(
-          ({ word }) => word === translationInHistory
-        );
-        if (matchWord) {
-          console.log(`Word ${matchWord} found in User`);
-          await user.updateOne({
-            $inc: { 'history.$.requested': 1 },
-          });
-        } else {
-          console.log(`Word ${translationInHistory.from} not found in User`);
-          await user.updateOne({
-            $push: { history: { word: translationInHistory } },
-          });
-        }
-      } else {
-        console.log(`User ${from.id} not found in database. Creating new user`);
-        await User.create({
-          userId: from.id,
-          history: [{ word: translationInHistory }],
-        });
-      }
+      // if (user) {
+      //   console.log(`User ${user.id} found in database`);
+      //   const matchWord = user.history.find(
+      //     ({ word }) => word === translationInHistory
+      //   );
+      //   if (matchWord) {
+      //     console.log(`Word ${matchWord} found in User`);
+      //     await user.updateOne({
+      //       $inc: { 'history.$.requested': 1 },
+      //     });
+      //   } else {
+      //     console.log(`Word ${translationInHistory.from} not found in User`);
+      //     await user.updateOne({
+      //       $push: { history: { word: translationInHistory } },
+      //     });
+      //   }
+      // } else {
+      //   console.log(`User ${from.id} not found in database. Creating new user`);
+      //   await User.create({
+      //     userId: from.id,
+      //     history: [{ word: translationInHistory }],
+      //   });
+      // }
     } else {
       const { translation, errorMessage } = await translateText(text, {
         from: detectedLang,
